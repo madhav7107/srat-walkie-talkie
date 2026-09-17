70005d4344330cc577babad0940f042c20abc352
madhav7107 <164631126+madhav7107@users.noreply.github.com>
2026-09-17 15:48:26 +0530

    Deploy CH-2 Owner Private Channel (Nimeeshbhai, Kalpeshbhai, Madhav private voice) - sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'godown-walkie-v8';
+const CACHE_NAME = 'godown-walkie-v9';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './style.css',
-  './app.js?v=8',
+  './style.css?v=9',
+  './app.js?v=9',
   './manifest.json',
   './icon-192.png',
   './icon-512.png',
